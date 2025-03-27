d11f45e4a09102e7b9b2607f80f34e2ee49d9887
i-am-surovi <ssur.ovi03@gmail.com>
2025-03-27 22:01:59 +0600

    mongodb connected

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./utils/db.js"
-import userRoute from "./routes/user.route.js"
+
 
 dotenv.config()
 
 const app = express()
-
 const PORT = process.env.PORT || 3000
-
-// apis
-app.use("/api/v1/user", userRoute)
-
-http://localhost:8000/api/v1/user/register 
 
 app.listen(PORT, ()=>{
     connectDB()
     console.log(`Server listen at port ${PORT}`);
 })
+
