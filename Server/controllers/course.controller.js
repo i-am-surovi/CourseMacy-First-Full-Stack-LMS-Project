db32acdfa8e409e3705308b7eb096cecacb2f402
i-am-surovi <ssur.ovi03@gmail.com>
2025-04-03 20:27:15 +0600

    Course Updated

--- Server/controllers/course.controller.js
+++ Server/controllers/course.controller.js
@@ -1,4 +1,6 @@
 import { Course } from "../models/course.model.js";
+import cloudinary from "../utils/cloud.js";
+import getDataUri from "../utils/dataUri.js";
 
 export const createCourse = async(req, res) => {
     try {
@@ -43,7 +45,7 @@ export const getPublishedCourse = async(_, res)=>{
     } catch (error) {
         console.log(error)
         return res.status(500).json({
-            message:"Failed to get course",
+            message:"Failed to get published course",
             success:false
         })
     }
@@ -68,9 +70,62 @@ export const getCreatorCourses = async (req, res) => {
     } catch (error) {
         console.log(error)
         return res.status(500).json({
-            message:"Failed to get course",
+            message:"Failed to create course",
             success:false
         })
     }
 }
 
+export const editCourse = async (req, res) => {
+    try {
+        const courseId = req.params.courseId;
+        const {courseTitle, subTitle, description, category, courseLevel, CoursePrice} = req.body;
+        const file = req.file;
+
+        let course = await Course.findById(courseId);
+        if(!course){
+            return res.status(404).json({
+                message:"Course not found!"
+            })
+        }
+        let courseThumbnail;
+        if(file){
+            const fileUri = getDataUri(file)
+            courseThumbnail = await cloudinary.uploader.upload(fileUri)
+        }
+        const UpdateData = {courseTitle, subTitle, description, category, courseLevel, CoursePrice, courseThumbnail:courseThumbnail?.secure_url};
+        course = await Course.findByIdAndUpdate(courseId, UpdateData, {new:true});
+        return res.status(200).json({
+            success:true,
+            course,
+            message:"Course updated successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message:"Failed to update course",
+            success:false
+        })
+    }
+}
+
+export const getCourseById = async (req, res) => {
+    try {
+        const {courseId} = req.params;
+        const course = await Course.findById(courseId)
+        if(!course){
+            return res.status(404).json({
+                message:"Course not found!"
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            course
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to get course by id"
+        })
+    }
+}
--- Server/routes/course.route.js
+++ Server/routes/course.route.js
@@ -1,11 +1,14 @@
 import express from "express"
 import { isAuthenticated } from "../middleware/isAuthenticated.js"
-import { createCourse, getCreatorCourses, getPublishedCourse } from "../controllers/course.controller.js"
+import { createCourse, editCourse, getCourseById, getCreatorCourses, getPublishedCourse } from "../controllers/course.controller.js"
+import { singleUpload } from "../middleware/multer.js"
 
 const router = express.Router()
 
 router.route("/").post(isAuthenticated, createCourse)
 router.route("/published-courses").get(getPublishedCourse)
 router.route("/").get(isAuthenticated, getCreatorCourses)
+router.route("/:courseId").put(isAuthenticated, singleUpload, editCourse)
+router.route("/:courseId").get(isAuthenticated, getCourseById)
 
 export default router;
